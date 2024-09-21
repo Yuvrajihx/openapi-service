c3f3ecbddcfb46b824e8874e1a68206c5dbec1b6
Yuvrajihx <yuvraj.maharshi@ihx.in>
2024-09-21 14:56:26 +0530

    db prompt implementation added

--- config/db.py
+++ config/db.py
@@ -8,9 +8,13 @@ class MongoDBConfig:
         self.mongo_url = os.getenv("MONGO_URL")
         self.database_name = os.getenv("DATABASE_NAME")
         self.collection_name = os.getenv("COLLECTION_NAME")
+        self.prompt_collection = os.getenv("PROMPT_COLLECTION")
         self.client = MongoClient(self.mongo_url)
         self.db = self.client[self.database_name]
         self.collection = self.db[self.collection_name]
+        self.prompt = self.db[self.prompt_collection]
 
     def get_collection(self):
         return self.collection
+    def get_prompt(self):
+        return self.prompt
